f6ceefc07fff4755091055e843e9ad3c2458c8b0
schoolboy <damsamoh@gmail.com>
2025-12-21 21:13:47 +0100

    Tried to iterate sets which is already iterable by default

--- Day-08/iterating-sets.js
+++ Day-08/iterating-sets.js
@@ -1,17 +1,46 @@
-function createIterator(set){
-    let count = 0;
-    return{
-        next: function(){
-            return count < set.size?
-            {value: set[count++], done: false}:
-            {value: undefined, done: true}
-        }
-
-        }
-    }
-
-let names =  createIterator(ab);
-
-console.log(names);
-
 // created a set named ab on a broswer console and tried to alert the names of the elements of the sets. will continue tomorrow.
+
+let ab = new Set();
+// Declaring Objects that'll be added into the set.
+
+let emmanuel = {
+    name: "Emmanuel Adebayo",
+    age: 18,
+    favfood: "Ewa Agoyin"
+};
+
+
+let damian = {
+    name: "Adebayo Damilola",
+    age: 20,
+    favfood: "Special Fried-rice"
+};
+
+
+// Adds in the object into the set
+
+ab.add(emmanuel).add(damian);
+
+// The fun part: Making the objects in the set iterable.
+
+ab[Symbol.iterator] = function(){
+    //Declaring the needed variables
+    let names = Set.prototype.values(ab);
+    let count = 0;
+    let isDone = false;
+
+    let next = () => {
+        if(count >= names.length){
+            isDone = true;
+        }
+        return{done: isDone, value: this[names[count++]]};
+    }
+    return{next};
+}
+
+for ( let x of ab){
+    alert(x);
+};
+
+
+// AirBall! 🤣
