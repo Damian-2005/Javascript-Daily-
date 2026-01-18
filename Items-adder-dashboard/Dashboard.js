1ad85cfdd1128f757ddb7106fad45e99fe5284dc
schoolboy <damsamoh@gmail.com>
2026-01-18 21:10:26 +0100

    Fixed the undefined bug encountered when adding a new task

--- Items-adder-dashboard/Dashboard.js
+++ Items-adder-dashboard/Dashboard.js
@@ -5,26 +5,25 @@ console.log("Hello");
 // Calling the forms
 
 let form = document.getElementById('forms');
-let tasksList = document.getElementById('task');
+let tasksList = document.getElementById('tasks');
 
-// form.addEventListener('submit', addNewTask);
+form.addEventListener('submit', addNewTask);
 
 // Declaration of function the adds the tasks.
 
 function addNewTask(e) {
     e.preventDefault();
-    let newTask = document.getElementById('task').value;
+    let newTask = document.querySelector('#taskss').value;
     let li = document.createElement('li');
     li.className = 'task';
     li.appendChild(document.createTextNode(newTask));
 
     if (newTask === "") return;
-
+    // Creating the checkbox for the new task 
     let checkbox = document.createElement('input');
     checkbox.type ='checkbox';
-    li.appendChild(checkbox);
+    li.appendChild(checkbox); 
 
-    tasksList.appendChild(li);
+    tasksList.appendChild(li); // This adds the new task to the existing tasks list
+
 }
-
-form.addEventListener('submit', addNewTask);
